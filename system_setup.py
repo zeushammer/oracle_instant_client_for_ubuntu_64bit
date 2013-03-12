73da4385d1d30dabe5a7ed3cb5eb080028112e8c
Michael Dunn <mike@eikonomega.com>
2013-03-12 11:45:49 -0400

    Add creation/modification of various configuration files.
    Create:
    /etc/ld.so.conf.d/oracle.conf
    /etc/profile.d/oracle.sh
    
    Modify:
    $HOME/.bashrc

--- system_setup.py
+++ system_setup.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 import os
+import sys
 import subprocess
 
 class SystemSetup(object):
 	def __init__(self):
-		pass
+		print (
+			'###############################################################\n'
+			'This program will install and configure the Oracle Instant\n'
+			'Client on your system.\n\n'
+			'You must run this program as root for it to work correctly.\n\n'
+			'Press enter to continue.\n'
+			'###############################################################\n'
+			)
+
+		raw_input()
 
 	def install_curl(self):
 		subprocess.check_call(["sudo", "apt-get", "install", "curl"])	
@@ -13,32 +23,9 @@ class SystemSetup(object):
 		subprocess.check_call(["sudo", "apt-get", "install", "alien"])
 
 	def oracle_setup(self):
-		self.install_curl()
+		# todo: Remove sudo from all calls. Require the user to invoke as root.
 		self.install_alien()
-
-		# subprocess.check_call(
-		# 	['sudo',
-		# 	'curl',
-		# 	'-vL',
-		# 	'-o',
-		# 	'/opt/oracle_libraries_primary_package.rpm',
-		# 	'https://notredame.box.com/shared/static/p1gdmp4b1t0lcg2cm42g.rpm'])
-
-		# subprocess.check_call(
-		# 	['sudo',
-		# 	'curl',
-		# 	'-vL',
-		# 	'-o',
-		# 	'/opt/oracle_libraries_supplemental_sqlplus.rpm',
-		# 	'https://notredame.box.com/shared/static/1qjgd9kx7pcfq4t2lptw.rpm'])
-
-		# subprocess.check_call(
-		# 	['sudo',
-		# 	'curl',
-		# 	'-vL',
-		# 	'-o',
-		# 	'/opt/oracle_libraries_supplemental_sdk.rpm',
-		# 	'https://notredame.box.com/shared/static/kgyw7tcmqi8yk1mlfij9.rpm'])
+		subprocess.check_call(["sudo", "apt-get", "install", "libaio1"])
 
 		subprocess.check_call(
 			['sudo',
@@ -52,11 +39,38 @@ class SystemSetup(object):
 			'-iv',
 			'oracle_rpms/oracle-instantclient11.2-sqlplus-11.2.0.3.0-1.x86_64.rpm'])
 
+		oracle_configuration_file = open('/etc/ld.so.conf.d/oracle.conf', 'w')
+		oracle_configuration_file.write('/usr/lib/oracle/11.2/client64/lib\n')
+		oracle_configuration_file.close()
+
+		subprocess.check_call(["sudo", "ldconfig"])
+
+		system_environment_vars = open('/etc/profile.d/oracle.sh', 'w')
+		system_environment_vars.write(
+			'export ORACLE_HOME=/usr/lib/oracle/11.2/client64\n')
+		system_environment_vars.write(
+			'export TNS_ADMIN=/usr/lib/oracle/11.2/client64/network/admin\n')
+		system_environment_vars.close()
+
+		os.mkdir('/usr/lib/oracle/11.2/client64/network/admin')
+
+		home_directory = os.environ['HOME']
+		bash_profile = open(home_directory + '.bashrc', 'a')
+		bash_profile.write(
+			'export LD_LIBRARY_PATH=/usr/lib/oracle/11.2/client64/lib\n')
+		bash_profile.write(
+			'export ORACLE_HOME=/usr/lib/oracle/11.2/client64\n')
+		bash_profile.write(
+			'export TNS_ADMIN=/usr/lib/oracle/11.2/client64/network/admin\n')
+		bash_profile.close()
+
+
+
 	def python_setup(self):
 		pass
 
-	def ruby_setup(self):
-		pass
+	def ruby_and_rails_setup(self):
+		subprocess.check_call(["sudo", "apt-get", "install", "rails"])
 
 	def install_git(self):
 		subprocess.check_call(["sudo", "apt-get", "install", "git"])	
